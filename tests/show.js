fbc7b7855f2d90d9a12e5121072c2b7856324db0
Juni Yadi <juniyadi94@gmail.com>
2019-10-16 19:44:26 +0700

    Added Test Show DynamoDB

---
+++ tests/show.js
@@ -0,0 +1,47 @@
+const AWS = require('aws-sdk')
+const dotenv = require('dotenv')
+dotenv.config()
+
+if(process.env.DYNAMODB_TYPE == 'local') {
+    AWS.config.update({
+        accessKeyId: 'dynamoDBLocal',
+        secretAccessKey: 'secret',
+        region: 'us-west-2',
+        endpoint: 'http://localhost:8000'
+    })
+} else {
+    AWS.config.update({
+        accessKeyId: process.env.DYNAMODB_KEY,
+        secretAccessKey: process.env.DYNAMODB_SECRET,
+        region: process.env.DYNAMODB_REGION
+    })
+}
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+const show = (table, id) => {
+    let params = {
+        TableName: table,
+        Key: {
+            id: id
+        }
+    }
+
+    return new Promise((resolve, reject) => {
+        docClient.get(params, (err, data) => {
+            if(err) {
+                reject(new Errror(err))
+            }
+
+            resolve(data)
+        })
+    })
+}
+
+show('ContentMetadata', '4dbd0fbf-0603-47d4-a98e-c19925bb0f5a')
+.then((items) => {
+    console.log(items)
+})
+.catch((err) => {
+    console.error(items)
+})
