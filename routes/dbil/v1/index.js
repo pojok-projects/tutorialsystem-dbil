01ad893661cd7e8f793557fbee90df83473ca096
Juni Yadi <juniyadi94@gmail.com>
2019-10-24 21:23:07 +0700

    Update Routes User

--- routes/dbil/v1/index.js
+++ routes/dbil/v1/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const v1 = express()
 
 const content = require('./content')
+const user = require('./user')
 
 v1.use('/content', content)
+v1.use('/user', user)
 
 module.exports = v1
