b555873274496ce680891a118c70ecf461a6484a
Juni Yadi <juniyadi94@gmail.com>
2019-10-24 19:56:56 +0700

    Update Routes Subtitle

--- routes/dbil/v1/content/index.js
+++ routes/dbil/v1/content/index.js
@@ -2,7 +2,11 @@ const express = require('express')
 const content = express()
 
 const metadata = require('./metadata')
+const category = require('./category')
+const subtitle = require('./subtitle')
 
 content.use('/metadata', metadata)
+content.use('/category', category)
+content.use('/subtitle', subtitle)
 
 module.exports = content
