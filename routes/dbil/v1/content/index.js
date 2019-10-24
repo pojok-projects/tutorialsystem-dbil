ecd7089b9439ddaf90e335d2f6f348c960d9e554
Juni Yadi <juniyadi94@gmail.com>
2019-10-24 20:29:08 +0700

    Added Routes Playlists Category

--- routes/dbil/v1/content/index.js
+++ routes/dbil/v1/content/index.js
@@ -4,9 +4,11 @@ const content = express()
 const metadata = require('./metadata')
 const category = require('./category')
 const subtitle = require('./subtitle')
+const playlistscategory = require('./playlistcategory')
 
 content.use('/metadata', metadata)
 content.use('/category', category)
 content.use('/subtitle', subtitle)
+content.use('/playlists/category', playlistscategory)
 
 module.exports = content
